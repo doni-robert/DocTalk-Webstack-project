ffc3e762afddb4447788d3469bb703880e62692f
R-Owino <sayeakinyi@gmail.com>
2023-12-26 23:10:29 +0300

    refactor: chat message routes

--- Backend/app/chat_message_routes.py
+++ Backend/app/chat_message_routes.py
@@ -1,37 +1,115 @@
 #!/usr/bin/env python3
 """ Routes for chat messages """
 
-from flask import Blueprint
-from views.chat_message import (
-    create_message,
-    get_messages,
-    delete_message,
-    update_message
-)
+from flask import Blueprint, request, jsonify, make_response
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from functools import wraps
+from datetime import datetime
+from models.chat_message import ChatMessage
+from models.chat_room import ChatRoom
+from models.user import User
+
+chats_bp = Blueprint('chat_message_routes', __name__, url_prefix='/chats')
 
 
-chat_message_bp = Blueprint('chat_message_routes', __name__, url_prefix='/chat_message')
+# user must be logged in to chat
+def logged_in(func):
+    @wraps(func)
+    @jwt_required()
+    def wrapper(*args, **kwargs):
+        current_user = get_jwt_identity()
+        return func(current_user, *args, **kwargs)
+    return wrapper
 
 
-@chat_message_bp.route("/message", methods=["POST"])
-def create():
-    """ creates a new message """
-    return create_message()
+@chats_bp.route('/send_message', methods=['POST'])
+@logged_in
+def send_message(current_user):
+    """ Sends a message to a chat room """
+    data = request.get_json()
+
+    message = data.get('message')
+    room_name = data.get('room_name')
+    receiver_name = data.get('receiver_name')
+
+    if not message or not room_name or not receiver_name:
+        return make_response(
+            jsonify(
+                {"message": "Missing message, room name or receiver name"}),
+            400)
+
+    room = ChatRoom.get_room_by_name(room_name)
+    if not room:
+        return make_response(
+            jsonify({"message": "Room not found"}),
+            404)
+
+    receiver = User.get_user_by_name(receiver_name)
+    if not receiver:
+        return make_response(
+            jsonify({"message": "Receiver not found"}),
+            404)
+
+    new_message = ChatMessage.create_message(message, current_user, receiver, room)
+    return make_response(
+        jsonify({"message": "Message sent successfuly"}),
+        201)
 
 
-@chat_message_bp.route("/message/<chat_room_id>", methods=["GET"])
-def get(chat_room_id):
-    """ get messages for a specific chat room """
-    return get_messages(chat_room_id)
+@chats_bp.route('/get_messages/<chat_room_name>', methods=['GET'])
+@logged_in
+def get_messages(current_user, chat_room_name):
+    """ Gets all messages from a chat room """
+    # check if chatroom exists and the user is part of it
+    room = ChatRoom.get_room_by_name(chat_room_name)
+    if room is None or current_user not in room.users:
+        return make_response(
+            jsonify({"message": f"Room not found or user not in room"}),
+            404)
+
+    # get all messages from the chat room
+    messages = ChatMessage.get_messages_by_room(chat_room_name)
+
+    # send the messages back
+    return jsonify(
+        {"messages": [message.to_dict() for message in messages]}), 200
 
 
-@chat_message_bp.route("/message/<message_id>", methods=["PUT"])
-def update(message_id):
-    """ edit a message """
-    return update_message(message_id)
+@chats_bp.route('/update/<message_id>', methods=['POST'])
+@logged_in
+def update(current_user, message_id):
+    """ Updates a message in a chat room """
+    # check if user exists and the message is from them
+    message = ChatMessage.objects(id=message_id).first()
+    if message is None or message.sender != current_user:
+        return make_response(
+            jsonify({"message": f"Message not found or not from user"}),
+            404)
+
+    # update the message
+    data = request.get_json()
+    message.message = data.get('message')
+    message.updated_at = datetime.now()
+    message.save()
+
+    # send the updated message back
+    return jsonify({"message": f"Message updated successfuly"}), 200
 
 
-@chat_message_bp.route("/message/<message_id>", methods=["DELETE"])
-def delete(message_id):
-    """ delete a message """
-    return delete_message(message_id)
+@chats_bp.route('/delete/<message_id>', methods=['DELETE'])
+@logged_in
+def delete(current_user, message_id):
+    """ Deletes a message in a chat room """
+    # check if user exists and the message is from them
+    message = ChatMessage.objects(id=message_id).first()
+    if message is None or message.sender != current_user:
+        return make_response(
+            jsonify({"message": f"Message not found or not from user"}),
+            404)
+
+    # delete the message
+    message.deleted_by.append(current_user)
+    message.save()
+
+    # return success message
+    return jsonify({"message": f"Message deleted successfully"}), 200
