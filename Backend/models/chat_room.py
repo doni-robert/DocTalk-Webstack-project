3fa77b88dd4af70857b7714da631b34c795b3376
R-Owino <sayeakinyi@gmail.com>
2023-12-29 13:12:19 +0300

    update: added static methods used by the app routes

--- Backend/models/chat_room.py
+++ Backend/models/chat_room.py
@@ -5,23 +5,48 @@ from mongoengine import (
     Document,
     StringField,
     ReferenceField,
-    DateTimeField,
-    ListField
+    ListField,
+    DateTimeField
 )
+from datetime import datetime
 from models.user import User
 
 
 class ChatRoom(Document):
     """ Chat room in the database """
-
-    name = StringField(required=True)
-    users = ListField(ReferenceField('User'), required=True)
-    created_at = DateTimeField(required=True)
-    updated_at = DateTimeField(required=True)
-    messages = ListField(ReferenceField('ChatMessage'))
+    room_name = StringField(max_length=50, required=True, unique=True)
+    users = ListField(ReferenceField(User))
+    created_at = DateTimeField(default=datetime.now)
+    updated_at = DateTimeField(default=datetime.now)
 
     meta = {
         'collection': 'chat_rooms',
-        'indexes': ['name'],
+        'indexes': ['room_name'],
         'ordering': ['-created_at']
     }
+
+    @staticmethod
+    def create_room(room_name, user):
+        """ Creates a new chat room """
+        new_room = ChatRoom(room_name=room_name, users=[user])
+        new_room.save()
+
+        return new_room
+
+    @staticmethod
+    def get_room_by_name(room_name):
+        """ Retrieves a chat room based on a name """
+        if room_name:
+            room = ChatRoom.objects(room_name=room_name).first()
+
+            return room
+        return None
+
+    @staticmethod
+    def get_user_by_name(username):
+        """ Retrieves a user based on a name """
+        if username:
+            user = User.objects(username=username).first()
+
+            return user
+        return None
