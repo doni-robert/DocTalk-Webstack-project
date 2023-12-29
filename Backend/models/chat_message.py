3037fef79c68a53cbb590af86bd9f28a46dada0f
R-Owino <sayeakinyi@gmail.com>
2023-12-29 13:13:12 +0300

    update: added static methods used by the app routes

--- Backend/models/chat_message.py
+++ Backend/models/chat_message.py
@@ -2,6 +2,7 @@
 """ Chat message model """
 
 from mongoengine import Document, StringField, ReferenceField, DateTimeField
+from datetime import datetime
 from models.user import User
 
 
@@ -10,8 +11,8 @@ class ChatMessage(Document):
     message = StringField(required=True)
     sender = ReferenceField(User, required=True)
     receiver = ReferenceField(User, required=True)
-    created_at = DateTimeField(required=True)
-    updated_at = DateTimeField(required=True)
+    created_at = DateTimeField(default=datetime.now)
+    updated_at = DateTimeField(default=datetime.now)
     chat_room = ReferenceField('ChatRoom', required=True)
 
     meta = {
@@ -19,3 +20,23 @@ class ChatMessage(Document):
         'indexes': ['sender', 'receiver', 'chat_room'],
         'ordering': ['-created_at']
     }
+
+    @staticmethod
+    def create_message(message, sender, receiver, chat_room):
+        """ Creates a new message """
+        new_message = ChatMessage(
+            message=message,
+            sender=sender,
+            receiver=receiver,
+            chat_room=chat_room
+        )
+        new_message.save()
+
+        return new_message
+
+    @staticmethod
+    def get_messages_by_room(room_name):
+        """ Gets all messages from a chat room """
+        messages = ChatMessage.objects(chat_room=room_name)
+
+        return messages
