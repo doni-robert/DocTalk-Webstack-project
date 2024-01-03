14fbb43161a268ecebbd7dd893276227cb042d4d
R-Owino <sayeakinyi@gmail.com>
2024-01-03 17:33:56 +0300

    improvement: specify classes for each environment

--- Backend/config.py
+++ Backend/config.py
@@ -5,6 +5,29 @@ import os
 
 
 class Config:
-    """ Configuration class """
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'xyzxyz xyzxyz xyzxyz'
-    DB_HOST = os.environ.get('DB_HOST') or 'mongodb://localhost:27017/doctalk_db'
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'xyzxyz xyzxyz xyzxyz')
+
+
+class DevConfig(Config):
+    """ Development Configuration class """
+    MONGODB_SETTINGS = {
+        'db': os.environ.get("MONGODB_DB", "dev_db"),
+        'host': os.environ.get("MONGODB_HOST", "localhost"),
+        'port': os.environ.get("MONGODB_PORT", 27017)
+    }
+    DEBUG = True
+
+
+class ProdConfig(Config):
+    """ Production Configuration class """
+    pass
+
+
+class TestConfig(Config):
+    """ Testing Configuration class """
+    MONGODB_SETTINGS = {
+        'db': "test_db",
+        'host': "localhost",
+        'port': 27017
+    }
+    TESTING = True
