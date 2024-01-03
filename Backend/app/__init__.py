c675c83b09b8406f0fc512e1249cb360e1021281
R-Owino <sayeakinyi@gmail.com>
2024-01-03 17:27:39 +0300

    improvement: specify configs based on the environment variable

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
 """ Makes the app folder a package"""
 
+import os
 from flask import Flask
-from config import Config
+from config import DevConfig, ProdConfig, TestConfig
 from db import init_mongodb
 from flask_jwt_extended import JWTManager
 from . import auth, chat_room_routes, chat_message_routes
 
 
 app = Flask(__name__, instance_relative_config=True)
-app.config.from_object(Config)
+
+# Use the appropriate config based on the environment variable
+if os.environ.get("FLASK_ENV") == "development":
+    app.config.from_object(DevConfig)
+elif os.environ.get("FLASK_ENV") == "production":
+    app.config.from_object(ProdConfig)
+else:  # Default to testing environment
+    app.config.from_object(TestConfig)
 
 # app routes
 app.register_blueprint(auth.bp)
